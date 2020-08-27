ea83819d59226b33a07f39b1a6b02457ede42eb0
rajeshvasamsetty <rajesh.vasamsetty@gmail.com>
2020-08-27 19:44:43 +0530

    Update runtests.sh

--- SBI/runtests.sh
+++ SBI/runtests.sh
@@ -3,6 +3,8 @@ echo $NEXUS_HOSTNAME
 echo $NEXUS_USERNAME
 echo "This is Nexu hostname: $NEXUS_HOSTNAME"
 echo "This is Nexxuss username: $NEXUS_USERNAME"
+echo "This is Sonar Hostname: $SONAR_HOSTNAME"
+echo "This is Sonar Token: $SONAR_TOKEN"
 #mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy
 #mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy sonar:sonar -Dsonar.host.url=http://13.229.133.70:9000 -Dsonar.login=$SONAR_TOKEN -Dsonar.projectName=sonar-report
 mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy sonar:sonar -Dsonar.login=$SONAR_TOKEN -Dsonar.projectName=sonar-report
