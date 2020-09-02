b577853c2f6ae9d275c66ca527f779afd5766b93
rajeshvasamsetty <rajesh.vasamsetty@gmail.com>
2020-09-02 12:52:47 +0530

    Update runtests.sh

--- SBI/runtests.sh
+++ SBI/runtests.sh
@@ -5,6 +5,6 @@ echo "This is Nexu hostname: $NEXUS_HOSTNAME"
 echo "This is Nexxuss username: $NEXUS_USERNAME"
 echo "This is Sonar Hostname: $SONAR_HOSTNAME"
 echo "This is Sonar Token: $SONARUSER_TOKEN"
-#mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy
+mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy
 #mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy sonar:sonar -Dsonar.host.url=http://13.229.133.70:9000 -Dsonar.login=$SONAR_TOKEN -Dsonar.projectName=sonar-report
-mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy sonar:sonar -Dsonar.login=$SONARUSER_TOKEN -Dsonar.issuesReport.html.enable=true
+#mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy sonar:sonar -Dsonar.login=$SONARUSER_TOKEN -Dsonar.issuesReport.html.enable=true
